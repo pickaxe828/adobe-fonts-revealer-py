a941f17a0a542b6ebd3b726f4906e8243c1a2353
Pickaxe828 <joshuajcn828@gmail.com>
2021-12-15 03:13:51 +0800

    Remove temp folder transfer

--- reveal.py
+++ reveal.py
@@ -20,38 +20,31 @@ print(f"Are you sure:")
 print(f"From: {fr}")
 print(f"To:   {to}")
 if input("(Y/N): ") not in ["y", "Y"]:
-    print("Script ended.")
+    print("Script exited.")
     exit()
 
 print()
-print("Initializing...")
 
 # List of ids
 fr_files_id = os.listdir(fr)
 
-# Try to make temp dir
-try:
-    os.mkdir("temp")
-except:
-    pass
-
 # Loop through ids and copy them to temp dir
 print("Copying files and changing names...")
 for file in tqdm(fr_files_id):
-    shutil.copy(os.path.join(fr, file), f"./temp/{file}.otf")
+    shutil.copy(os.path.join(fr, file), f"{to}/{file}.otf")
 
     # Get the font details
     t = subprocess.Popen(
-        f"./otfinfo --info ./temp/{file}.otf", stdout=subprocess.PIPE, universal_newlines=True, encoding="utf-8")
+        f"./otfinfo --info {to}/{file}.otf", stdout=subprocess.PIPE, universal_newlines=True, encoding="utf-8")
 
     # Loop through every line
     for i in t.stdout.read().splitlines():
         # Extract the font family name
         if i.find("PostScript name:") != -1:
             # Copy the font from the temp dir to the final dir
-            shutil.copy(f"./temp/{file}.otf", f"{to}/{i.split('     ')[1]}.otf")
+            shutil.copy(f"{to}/{file}.otf", f"{to}/{i.split('     ')[1]}.otf")
+            os.remove(f"{to}/{file}.otf")
             break
 
 # Remove the temp file
-print("Removing temp dir...")
-shutil.rmtree("./temp")
+print(f"Done copying {len(fr_files_id)} files!")
